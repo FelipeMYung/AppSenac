60ae964a64123407d1ff709bf02e1133b588d82f
FelipeMYung <102309048+FelipeMYung@users.noreply.github.com>
2024-07-10 11:29:28 -0300

    add act

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,63 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnTecnico = findViewById(R.id.btnTecnico);
+        btnTecnico.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent tecnico = new Intent( getApplicationContext(), TecnicoActivity.class);
+                startActivity(tecnico);
+            }
+        });
+        Button btnLinguagens = findViewById(R.id.btnLinguagens);
+        btnLinguagens.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent tecnico = new Intent( getApplicationContext(), TecnicoActivity.class);
+                startActivity(tecnico);
+            }
+        });
+        Button btnCN = findViewById(R.id.btnCN);
+        btnLinguagens.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent CN = new Intent( getApplicationContext(), TecnicoActivity.class);
+                startActivity(CN);
+            }
+        });
+        Button btnMat = findViewById(R.id.btnMat);
+        btnMat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent Mat = new Intent( getApplicationContext(), TecnicoActivity.class);
+                startActivity(Mat);
+            }
+        });
+        Button btnPP = findViewById(R.id.btnPP);
+        btnPP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent PP = new Intent( getApplicationContext(), TecnicoActivity.class);
+                startActivity(PP);
+            }
+        });
+        Button btnPV = findViewById(R.id.btnPV);
+        btnPV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent PV = new Intent( getApplicationContext(), TecnicoActivity.class);
+                startActivity(PV);
+            }
+        });
+        Button btnCH = findViewById(R.id.btnCH);
+        btnCH.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent CH = new Intent( getApplicationContext(), TecnicoActivity.class);
+                startActivity(CH);
+            }
+        });
     }
+
 }
